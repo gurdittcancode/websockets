497f6796e45f06b88e7f565c29cf58e0d212727f
gurditt singh <gurdittsingh1508@gmail.com>
2024-09-28 00:38:22 +0530

    random stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,39 +2,39 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-    const [socket, setSocket] = useState<WebSocket | null>(null);
-    const [message, setMessage] = useState("");
-    const [latestMessage, setLatestMessage] = useState("");
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [message, setMessage] = useState("");
+  const [latestMessage, setLatestMessage] = useState("");
 
-    useEffect(() => {
-        const socket = new WebSocket("ws://localhost:8080");
-        socket.onopen = () => {
-            console.log("Connected");
-            setSocket(socket);
-        };
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+    socket.onopen = () => {
+      console.log("Connected");
+      setSocket(socket);
+    };
 
-        socket.onmessage = (message) => {
-            console.log("Received message: ", message.data);
-            setLatestMessage(message.data);
-        };
+    socket.onmessage = (message) => {
+      console.log("Received message: ", message.data);
+      setLatestMessage(message.data);
+    };
 
-        return () => {
-            socket.close();
-        };
-    }, []);
+    return () => {
+      socket.close();
+    };
+  }, []);
 
-    if (!socket) {
-        return <div>Connecting to socket</div>;
-    }
-    return (
-        <div>
-            <div>
-                <input onChange={(e) => setMessage(e.target.value)} />
-                <button onClick={() => socket.send(message)}>Send</button>
-            </div>
-            <div>{latestMessage}</div>
-        </div>
-    );
+  if (!socket) {
+    return <div>Connecting to socket</div>;
+  }
+  return (
+    <div>
+      <div>
+        <input onChange={(e) => setMessage(e.target.value)} />
+        <button onClick={() => socket.send(message)}>Send</button>
+      </div>
+      <div>{latestMessage}</div>
+    </div>
+  );
 }
 
 export default App;
